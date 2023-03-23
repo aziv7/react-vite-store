e85e4cd5338f0c6bfb3387e502ac289ffc06d203
aziv7 <57321183+aziv7@users.noreply.github.com>
2023-03-24 00:40:00 +0100

    select categorie item

--- src/components/ProductCategries.jsx
+++ src/components/ProductCategries.jsx
@@ -1,6 +1,14 @@
-import React, { memo } from 'react';
+import React, { memo, useState } from 'react';
 import { BsSearch } from 'react-icons/bs';
 const ProductCategries = () => {
+  const [categories, setCategories] = useState([
+    'Bed',
+    'Chair',
+    'Sofa',
+    'Recliner',
+  ]);
+  const [selected, setSelected] = useState(0);
+
   return (
     <div className='mt-5 mx-5'>
       <div className=' flex items-center border rounded-full p-2 hover:border-gray-600 border-gray-300 space-x-3'>
@@ -13,6 +21,20 @@ const ProductCategries = () => {
           id='search'
           type='text'
         />
+      </div>
+      <div className='flex space-x-3 py-5 px-2 my-5 overflow-x-auto'>
+        {categories.map((c, i) => (
+          <button
+            key={c}
+            onClick={() => setSelected(i)}
+            className={`bg-gradient-to-tr transition-all duration-200 ease-in-out ${
+              selected == i
+                ? 'from-black to-gray-500 text-white'
+                : 'from-gray-400 to-gray-100 text-black'
+            }  rounded-full px-6 py-2.5`}>
+            {c}
+          </button>
+        ))}
       </div>
     </div>
   );
