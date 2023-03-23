bedc74736eaff18c9c541d310c8024cdc75f1412
aziv7 <57321183+aziv7@users.noreply.github.com>
2023-03-24 00:23:47 +0100

    filter search input

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './components/Header';
 import Home from './components/Home';
+import ProductCategries from './components/ProductCategries';
 
 function App() {
   return (
     <div className='App'>
       <Header />
       <Home />
+      <ProductCategries />
     </div>
   );
 }
--- src/components/ProductCategries.jsx
+++ src/components/ProductCategries.jsx
@@ -1,7 +1,21 @@
 import React, { memo } from 'react';
-
+import { BsSearch } from 'react-icons/bs';
 const ProductCategries = () => {
-  return <div>ProductCategries</div>;
+  return (
+    <div className='mt-5 mx-5'>
+      <div className=' flex items-center border rounded-full p-2 hover:border-gray-600 border-gray-300 space-x-3'>
+        <span>
+          <BsSearch className='text-gray-500' size={22} />
+        </span>
+        <input
+          className='outline-none'
+          placeholder='Search'
+          id='search'
+          type='text'
+        />
+      </div>
+    </div>
+  );
 };
 
 export default memo(ProductCategries);
