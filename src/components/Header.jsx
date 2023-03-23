fe7c3cc73edc1b90f6f07dac2bc382a3d35494dc
aziv7 <57321183+aziv7@users.noreply.github.com>
2023-03-24 00:10:39 +0100

    header & home components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,21 @@
 import React, { memo } from 'react';
-
+import { BiMenuAltRight } from 'react-icons/bi';
 const Header = (props) => {
-  return <div>Header</div>;
+  return (
+    <div className=' px-5 py-5 flex justify-between items-center'>
+      <span className='cursor-pointer'>
+        <BiMenuAltRight size={'2.5rem'} />
+      </span>
+
+      <div className='bg-gray-200 w-[2.75rem] h-[2.75rem] rounded-full cursor-pointer'>
+        <img
+          src='https://images.unsplash.com/photo-1679412330029-66dbd3365fd8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+          alt=''
+          className='rounded-full w-full h-full object-cover'
+        />
+      </div>
+    </div>
+  );
 };
 
 export default memo(Header);
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,20 @@
 import React, { memo } from 'react';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className='px-5'>
+      <h1 className='text-[1.65rem] font-semibold leading-tight'>
+        Choose Your
+      </h1>
+      <h1 className='text-[1.65rem] font-semibold leading-tight'>
+        Best Furniture
+      </h1>
+      <p className='text-sm text-gray-500 mt-1'>
+        {' '}
+        Your perfect furniture is here.
+      </p>
+    </div>
+  );
 };
 
 export default memo(Home);
